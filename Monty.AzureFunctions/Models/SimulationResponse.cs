2cb44ab267f9d5e837edbc2ed6dd2d5e26572d41
Edward Aschan <edward.aschan@scandichotels.com>
2020-04-24 22:16:03 +0200

    Added frontend (Azure Function)

---
+++ Monty.AzureFunctions/Models/SimulationResponse.cs
@@ -0,0 +1,30 @@
+﻿namespace Monty.AzureFunctions.Models
+{
+    using System;
+
+    using Monty.Simulations;
+
+    public class SimulationResponse : ISimulationResponse
+    {
+        public SimulationResponse(ISimulationResult result)
+        {
+            if (result==null)
+            {
+                throw new ArgumentNullException(nameof(result));
+            }
+
+            ExecutionDate = result.ExecutionDate;
+            NumberOfGames = result.NumberOfGames;
+            SwitchDoor = result.SwitchDoor;
+            WinPercentage = result.WinPercentage;
+        }
+
+        public DateTimeOffset ExecutionDate { get; }
+
+        public int NumberOfGames { get; }
+
+        public bool SwitchDoor { get; }
+
+        public decimal WinPercentage { get; }
+    }
+}
