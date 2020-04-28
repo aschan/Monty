b8df069b51705f566b46fc20e8c936a5bb3f3325
Edward Aschan <edward.aschan@scandichotels.com>
2020-04-28 08:57:11 +0200

    Moved random usage to constructor to avoid defered execution issues

--- Monty.Simulations/InformedHost/InformedHostGame.cs
+++ Monty.Simulations/InformedHost/InformedHostGame.cs
@@ -1,24 +1,41 @@
 ﻿namespace Monty.Simulations.InformedHost
 {
     using System;
+    using System.Threading;
 
     public class InformedHostGame : IGame
     {
-        private readonly Random _randomGenerator;
+        private readonly int _luckyDoor;
+        private readonly int _selectedDoor;
 
-        public InformedHostGame(int id, Random randomGenerator)
+        public InformedHostGame(int id, ThreadLocal<Random> randomGenerator)
         {
+            if (id <= 0)
+            {
+                throw new ArgumentException(@"Value must be a positive integer", nameof(id));
+            }
+            
+            if (randomGenerator == null)
+            {
+                throw new ArgumentNullException(nameof(randomGenerator));
+            }
+
+            if (randomGenerator.Value == null)
+            {
+                throw new ArgumentException(@"Value must be initialized", nameof(randomGenerator));
+            }
+
             Id = id;
-            _randomGenerator = randomGenerator ?? throw new ArgumentNullException(nameof(randomGenerator));
+            _luckyDoor = randomGenerator.Value.Next(1, 4);
+            _selectedDoor = randomGenerator.Value.Next(1, 4);
         }
 
         public int Id { get; }
 
         public IGameResult Play(bool switchDoor)
         {
-            var luckyDoor = _randomGenerator.Next(1, 4);
-            var selectedDoor = _randomGenerator.Next(1, 4);
-            var result = (selectedDoor == luckyDoor && !switchDoor) || (selectedDoor != luckyDoor && switchDoor);
+            Console.WriteLine($"Game {Id}, swith door = {switchDoor}");
+            var result = (_selectedDoor == _luckyDoor && !switchDoor) || (_selectedDoor != _luckyDoor && switchDoor);
                 
             return new GameResult(Id, switchDoor, result);
         }
