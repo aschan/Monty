3bc304129d201e5896b9b56a7c11e28cc8ab8de1
Edward Aschan <edward.aschan@scandichotels.com>
2020-04-28 08:58:10 +0200

    Added ThreadLocal<Random> and PLINQ for game generation

--- Monty.Simulations/InformedHost/InformedHostSimulation.cs
+++ Monty.Simulations/InformedHost/InformedHostSimulation.cs
@@ -3,25 +3,27 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Threading;
 
     public class InformedHostSimulation : ISimulation
     {
-        private readonly Random _randomGenerator = new Random();
-
         public ISimulationResult Run(int numberOfGames, bool switchDoor)
         {
             var now = DateTimeOffset.UtcNow;
             var games = GenerateGames(numberOfGames);
-            var results = games.Select(g => g.Play(switchDoor));
+            var results = games.AsParallel().Select(g => g.Play(switchDoor)).ToList();
 
             return new SimulationResult(now, switchDoor, results);
         }
 
         private IEnumerable<IGame> GenerateGames(int numberOfGames)
         {
-            foreach (var id in Enumerable.Range(1, numberOfGames))
+            using (var random = new ThreadLocal<Random>(() => new Random(Guid.NewGuid().GetHashCode())))
             {
-                yield return new InformedHostGame(id, _randomGenerator);
+                return Enumerable.Range(1, numberOfGames)
+                                 .AsParallel()
+                                 .Select(i => new InformedHostGame(i, random))
+                                 .ToList();
             }
         }
     }
